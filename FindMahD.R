5b6e883536e1c1bac579d480b27e60a4dd654092
James Smith <38435316+edSmith62@users.noreply.github.com>
2018-10-03 06:55:58 -0700

    Update FindMahD.R

--- FindMahD.R
+++ FindMahD.R
@@ -12,7 +12,7 @@ library(DescTools)
 #Function multiplies T2 by the inverse of the weighting to get 
 #Mahalanobis distance for T2 test or paired T2 tests
 
-MahEff <- function(data = data, g1 = NULL, g2 = NULL){
+MahEff <- function(data = NULL, g1 = NULL, g2 = NULL){
 
 	if(is.null(g1) == TRUE & is.null(g2) == TRUE){
 	q <- ncol(data); n <- nrow(data)
@@ -21,8 +21,8 @@ MahEff <- function(data = data, g1 = NULL, g2 = NULL){
 	return(EffectSize)
 	}
 	else{ 
-	q <- ncol(data)
-	n <- nrow(data) - 1
+	q <- ncol(g1)
+	n <- nrow(g1)+nrow(g2) - 1
 	T2 <- HotellingsT2Test(x = g1, y = g2 , test = "f")$statistic
 	EffectSize <-((q*(n-1))/(n-q))* T2
 	}
